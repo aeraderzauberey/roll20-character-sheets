5423b7e0e07cb2160c1272673a961cef2db68a41
Jens Bannmann <jens.b@web.de>
2021-05-11 21:37:24 +0200

    Add toggle buttons for applying penalty

--- Aera der Zauberey/src/sheetworker/main.js
+++ Aera der Zauberey/src/sheetworker/main.js
@@ -1,6 +1,9 @@
-
-
-
-
+on("clicked:toggle_apply_penalty", () => {
+    getAttrs(["apply_penalty"], (v) => {
+        setAttrs({
+            "apply_penalty": v["apply_penalty"] !== "1" ? "1" : "0"
+        });
+    });
+});
 
 console.log("AdZ sheetworker initialized");
